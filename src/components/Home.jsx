c5da12dbbc1e05262ffaff99e51d28043ec1a82a
nazar301 <nazar.basistyy@gmail.com>
2023-01-26 21:42:31 -0500

    added style-componenets

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,36 @@
 import React from "react";
+import { Header, Section, H1, Paragraph, StyledButton, Button, BigImgContainer, SectionP} from "./Container.styled";
 
 const Home = () => {
   return (
     <div>
-      <header><img src={require('../assets/noBgBlack.png')} alt="Love and Service 1rst Logo" /></header>
-      <section>
-        <h1>Love in action</h1>
-        <p>
-          Transforming lives by providing homeless individuals with the
-          resources and support they need to achieve self-sufficiency and break
-          the cycle of poverty.
-        </p>
-        <button>DONATE TODAY</button>
-      </section>
-     {/* <img src="" alt="" /> */}
-      <section>
-        "Pay it Forward" is an initiative started in Las Vegas to help the
-        homeless population get back on their feet. - LAS1 team
-      </section>
+      <Header><img src={require('../assets/noBgBlack.png')} width={200} height={155.81}  alt="Love and Service 1rst Logo" /></Header>
+      <Section>
+          <div>  
+              <H1>Love in action</H1>
+              <Paragraph>
+              Transforming lives by providing homeless individuals with the
+              resources and support they need to achieve self-sufficiency and break
+              the cycle of poverty.
+              </Paragraph>
+              
+              <StyledButton>
+                DONATE TODAY
+              </StyledButton>
+              
+            </div>
+        </Section>
+      <BigImgContainer>
+        <img src={require('../assets/heartimg.jpg')} width={"100%"} alt="Big Heart" />
+      </BigImgContainer>
+      <SectionP>
+        <div style={{margin: "0rem 10rem"}}>
+          <div>"Pay it Forward" is an initiative started in Las Vegas to help the
+           homeless population get back on their feet. 
+           <div style={{fontSize: "17px", fontFamily: "Noto Serif" }}>- LAS1 team</div>
+          </div>
+        </div>
+      </SectionP>
       <footer>
         {/* <img src="" alt="" srcset="" /> */}
         <a href="#">Facebook</a>
